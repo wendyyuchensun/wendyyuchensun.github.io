6ed41420326f299389dfc64e042c3b2933fc255b
ysun01 <wendy.sun@verizonmedia.com>
2021-03-16 00:50:14 +0800

    Rm outdated content & update config

--- script.js
+++
@@ -1,9 +0,0 @@
-// Video-container RWD
-const vc = document.getElementsByClassName("video-container");
-
-if (vc.length) {
-  const vcHeight = vc[0].offsetWidth * 315 / 560 + "px";
-  for (let i = 0; i < vc.length; i++) {
-    vc[i].style.height = vcHeight;
-  };
-};
